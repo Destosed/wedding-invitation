0812f9ac554cbb439409b60a81536be5343353a4
Nikita <nikita.luzhbin@synchro.pro>
2024-05-08 13:55:28 +0300

    Added splash, wide screen and location changes

--- scripts/index.js
+++ scripts/index.js
@@ -34,7 +34,37 @@ window.onload = function() {
 
 document.addEventListener("DOMContentLoaded", function() {
 
+  // Show wide screen view
+  const isWideMonitor = window.innerHeight < window.innerWidth;
+  const wideScreenView = document.getElementById('wide-screen-view');
+
+  console.log(`Launch on wide screen: ${isWideMonitor}`)
+
+  if (isWideMonitor) {
+    document.getElementById('main-content').style.display = 'block';
+  } else {
+    document.getElementById('wide-screen-view').style.display = 'none';
+  }
+
+  // Show splash screen
+  const splashScreen = document.getElementById('splash-screen');
+  splashScreen.style.display = 'flex'; // Show splash screen
+
+  // Hide splash screen when page is fully loaded
+  window.onload = function(e) { 
+    console.log("window.onload", Date.now());
+    splashScreen.style.display = 'none'; // Hide splash screen
+    document.getElementById('main-content').style.display = 'block'; // Show main content
+  }
+
   // MARK: - Add listener for button
+
+  var locationButton = document.getElementById("location-button");
+  locationButton.addEventListener("click", function() {
+    console.log('Location button tapped')
+    window.open('https://yandex.ru/maps/?ol=geo&text=%D1%83%D0%BB%D0%B8%D1%86%D0%B0%20%D0%90%D0%BB%D1%8C%D1%84%D1%80%D0%B5%D0%B4%D0%B0%20%D0%A5%D0%B0%D0%BB%D0%B8%D0%BA%D0%BE%D0%B2%D0%B0,%2026&sll=49.095939,55.781929&sspn=0.002000,0.002000')
+  });
+
   var submitButton = document.getElementById("submit-button");
 
   submitButton.addEventListener("click", function() {
